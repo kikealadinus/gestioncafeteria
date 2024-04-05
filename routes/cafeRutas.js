0608b52d1d5a5410e901c8fd2a3a4a469c9dc501
kikealadino <kikealadino02@gmail.com>
2024-04-05 10:50:01 -0400

    consultas avanzadas gestion cafeteria

--- routes/cafeRutas.js
+++ routes/cafeRutas.js
@@ -6,7 +6,7 @@ const CafeModel = require('../models/Cafe');
 rutas.get('/', async (req, res) => {
     try {
         const listacafes = await CafeModel.find();
-        console.log(listacafes);
+        //console.log(listacafes);
         res.json(listacafes);
     } 
     catch (error) {
@@ -53,6 +53,68 @@ rutas.delete('/eliminar/:id', async (req, res) => {
 
     } catch (error) {
         res.status(404).json({mensaje: error.mensaje});
+    }
+});
+
+//CONSULTAS
+//- Listar todas las tareas con prioridad 5
+rutas.get('/cafes-prioridad/:id', async (req, res) => {
+    try {
+        console.log(req.params.id);
+        const listacafesprioridad = await CafeModel.find({ prioridad: req.params.id });
+        res.json(listacafesprioridad);
+    } 
+    catch (error) {
+        res.status(404).json({mensanje: error.mensaje});
+    }
+});
+
+//- Ordenar las tareas por prioridad de forma ascendente
+rutas.get('/ordenar-cafes', async (req, res) => {
+    try {
+        console.log(req.params.id);
+        const cafeASC = await CafeModel.find().sort({prioridad: 1});
+        res.json(cafeASC);
+    } 
+    catch (error) {
+        res.status(404).json({mensanje: error.mensaje});
+    }
+});
+
+//- Consultar una tarea especifica por Id
+rutas.get('/cafes/:id', async (req, res) => {
+    try {
+        //console.log(req.params.id);
+        const cafe = await CafeModel.findById(req.params.id);
+        res.json(cafe);
+    } 
+    catch (error) {
+        res.status(404).json({mensanje: error.mensaje});
+    }
+});
+//- Eliminar todas las tareas con una prioridad determinada
+rutas.delete('/eliminar-prioridad/:prioridad', async (req, res) => {
+    try {
+        console.log(req.params.prioridad);
+        const prioridad = req.params.prioridad
+        const Elimnarcafes = await CafeModel.deleteMany({prioridad});
+        res.json({mensaje: 'Cafés eliminados correctamente'})
+        //res.json(cafe);
+    } 
+    catch (error) {
+        res.status(404).json({mensanje: error.mensaje});
+    }
+});
+
+//- Consultar la tarea mas reciente anadida a la base de datos
+rutas.get('/cafes-reciente', async (req, res) => {
+    try {
+        //console.log(req.params.id);
+        const cafe = await CafeModel.findOne().sort({_id: -1});
+        res.json(cafe);
+    } 
+    catch (error) {
+        res.status(404).json({mensanje: error.mensaje});
     }
 });
 
